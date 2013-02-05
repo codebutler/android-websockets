7ea8d981f15908829eb69c2d30e929276229f385
Mike Stewart <mike.stewart@unb.ca>
2013-02-05 00:49:29 -0400

    Create an SSL socket for https connections. Author: Mike Stewart

--- src/main/java/com/codebutler/android_websockets/WebSocketClient.java
+++ src/main/java/com/codebutler/android_websockets/WebSocketClient.java
@@ -72,7 +72,7 @@ public class WebSocketClient {
                 try {
                     String secret = createSecret();
 
-                    int port = (mURI.getPort() != -1) ? mURI.getPort() : (mURI.getScheme().equals("wss") ? 443 : 80);
+                    int port = (mURI.getPort() != -1) ? mURI.getPort() : ((mURI.getScheme().equals("wss") || mURI.getScheme().equals("https")) ? 443 : 80);
 
                     String path = TextUtils.isEmpty(mURI.getPath()) ? "/" : mURI.getPath();
                     if (!TextUtils.isEmpty(mURI.getQuery())) {
@@ -82,7 +82,7 @@ public class WebSocketClient {
                     String originScheme = mURI.getScheme().equals("wss") ? "https" : "http";
                     URI origin = new URI(originScheme, "//" + mURI.getHost(), null);
 
-                    SocketFactory factory = mURI.getScheme().equals("wss") ? getSSLSocketFactory() : SocketFactory.getDefault();
+                    SocketFactory factory = (mURI.getScheme().equals("wss") || mURI.getScheme().equals("https")) ? getSSLSocketFactory() : SocketFactory.getDefault();
                     mSocket = factory.createSocket(mURI.getHost(), port);
 
                     PrintWriter out = new PrintWriter(mSocket.getOutputStream());
